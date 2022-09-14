f5b5166002a36e53bf48676b67fef9ec0f941034
Andrey Loskutov <joofer.sp@gmail.com>
2022-09-14 23:13:32 +0300

    created new pages

---
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_pizzeria.Pages.Forms
+{
+    public class CustomPizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
