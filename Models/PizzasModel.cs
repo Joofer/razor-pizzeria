4686aaf58c9a975eecac98d9b3a3ac5ea588f341
Andrey Loskutov <joofer.sp@gmail.com>
2022-09-22 21:44:06 +0300

    edited pizza model

---
+++ Models/PizzasModel.cs
@@ -0,0 +1,34 @@
+﻿namespace razor_pizzeria.Models
+{
+    public class PizzasModel
+    {
+        #region general
+
+        public string ImageTitle { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; } = 2;
+        public float FinalPrice { get; set; }
+        public bool MakeVegie { get; set; }
+
+        #endregion
+
+        #region ingredients
+
+        public bool Pepperoni { get; set; }
+        public bool Bacon { get; set; }
+        public bool Ham { get; set; }
+        public bool Beef { get; set; }
+
+        public bool Mozzarella { get; set; }
+        public bool Cheddar { get; set; }
+
+        public bool Jalapeno { get; set; }
+        public bool Spices { get; set; }
+        public bool Mushroom { get; set; }
+        public bool Pineapple { get; set; }
+
+        public bool TomatoSauce { get; set; }
+
+        #endregion
+    }
+}
